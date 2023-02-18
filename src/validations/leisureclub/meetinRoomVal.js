86f28ab6f23d2dfd36aecdd22aeb0c0beae2762d
Tunar Alyarzada <79589389+alyarzada@users.noreply.github.com>
2023-02-18 10:46:02 +0400

    commit to admin

--- src/validations/leisureclub/meetinRoomVal.js
+++
@@ -1,8 +0,0 @@
-import * as Yup from "yup";
-
-export const BronMeetingRoomSchema = Yup.object().shape({
-  start_date: Yup.string().required(),
-  start_time: Yup.string().required(),
-  duration: Yup.string().required(),
-  meeting_room: Yup.string().required(),
-});
