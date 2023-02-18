7ffa649e2cce5722db4aa7ad2549c6c61a1f471e
Tunar Alyarzada <79589389+alyarzada@users.noreply.github.com>
2023-02-18 10:57:46 +0400

    commit to ruslan

--- src/validations/CreateProjectValidation.js
+++
@@ -1,7 +0,0 @@
-import * as Yup from "yup";
-
-export const ProjectSchema = Yup.object().shape({
-  projectName: Yup.string().required(),
-  aboutProject: Yup.string().required(),
-  teamMembers: Yup.array().of(Yup.object()).min(1).required(),
-});
--- src/validations/controlpanel/services.js
+++
@@ -1,6 +0,0 @@
-import * as Yup from "yup";
-
-export const ServicesSchema = Yup.object().shape({
-  title: Yup.string().required(),
-  message: Yup.string().required(),
-});
