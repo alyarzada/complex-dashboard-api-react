3f194dd061dba1a96b0f6a73f882b41c7d456828
Tunar Alyarzada <79589389+alyarzada@users.noreply.github.com>
2023-02-18 10:45:21 +0400

    commit to main

--- src/hooks/useScrollToUp.js
+++
@@ -1,7 +0,0 @@
-import { useEffect } from "react";
-
-export const useScrollToUp = () => {
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, []);
-};
--- src/i18n/locales/index.js
+++
@@ -1,3 +0,0 @@
-export * as en from "./en.json";
-export * as az from "./az.json";
-export * as rus from "./rus.json";
