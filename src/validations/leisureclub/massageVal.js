c4a52e155170bc8e6d99c7f4b9f9b8831f89f3f0
Tunar Alyarzada <79589389+alyarzada@users.noreply.github.com>
2023-03-07 12:32:18 +0400

    commit to admin

--- src/validations/leisureclub/massageVal.js
+++
@@ -1,8 +0,0 @@
-import * as Yup from "yup";
-
-export const BronMassageSchema = Yup.object().shape({
-  start_date: Yup.string().required(),
-  start_time: Yup.string().required(),
-  therapist: Yup.string().required(),
-  massages: Yup.string().required(),
-});
